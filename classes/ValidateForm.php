4fbe17d36887c8c587229798658337e935657ffb
petrkudrnovsky <kudrnovsky.p@gmail.com>
2020-11-19 17:51:58 +0100

    added input and class ValidateInput

---
+++ classes/ValidateForm.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types = 1);
+
+class ValidateForm {
+    private $surname;
+    private $lastname;
+    private $birth_number;
+    private $birthday;
+
+    public function __construct($surname, $lastname, $birth_number, $birthday){
+        $this->surname = $surname;
+        $this->lastname = $lastname;
+        $this->birth_number = $birth_number;
+        $this->birthday = $birthday;
+    }
+
+    private function validateNameInput($surname, $lastname) {
+        if(preg_match("/^([a-zA-Z]+)$/", $surname) && preg_match("/^([a-zA-Z]+)$/", $lastname)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private function validateBirthNumber($birth_number) {
+        if(preg_match("/^(/d{6}\//d{4})$/", $birth_number)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private function birthDateHandler($birth_number, $birthday) {
+        if($birth_number) {
+
+        }
+    }
+
+    public function printOutput($surname, $lastname) {
+        if(validateNameInput($surname, $lastname)) {
+
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    declare(strict_types = 1);
+    mb_internal_encoding("UTF-8");
+?>
+
+<!DOCTYPE html>
+<html lang="cs-cz">
+    <head>
+        <meta charset="UTF-8">
+        <title>Formulář</title>
+    </head>
+    <body>
+        <h1>Formulář</h1>
+        <form method="post">
+            <label for="surname">Jméno:</label><br>
+                <input type="text" id="surname" name="surname" required><br>
+            <label for="lastname">Příjmení:</label><br>
+                <input type="text" id="lastname" name="lastname" required><br>
+            <label for="birth_number">Rodné číslo: <i>(prosíme, zadejte ve formátu XXXXXX/XXXX)</i></label><br>
+                <input type="text" id="birth_number" name="birth_number"><br>
+            <label for="birthday">Datum narození</label><br>
+                <input type="date" id="birthday" name="birthday"><br>
+            <input type="submit" value="Odeslat formulář">
+        </form>
+
+        <?php
+            if($_POST) {
+
+            }
+        ?>
+    </body>
+</html>
